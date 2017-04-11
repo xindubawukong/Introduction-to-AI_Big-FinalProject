e515b60a765e5ffbb1007a13dbda06ea4c44f960
xindubawukong <xindubawukong@outlook.com>
2017-04-11 21:26:07 +0800

    first commit, AI0 added

---
+++ AI0.cpp
@@ -0,0 +1,9 @@
+#include "AI0.h"
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+pair<int, int> AI0_getPoint(const int M, const int N, const int* top, int** _board, const int lastX, const int lastY, const int noX, const int noY) {
+	return make_pair(0, 0);
+}
